53f0f9db2f0d6cd4830c0d6e3348a193658639c4
lxxhack <1572411660@qq.com>
2017-07-10 16:27:27 +0800

    Update practice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,38 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  for (var i=0;i<collection.length;i++)
+{
+if (collection[i].charAt(1)=='-'||collection[i].charAt(1)=='['||collection[i].charAt(1)==':')
+{
+var ch=collection[i].charAt(0);
+var num=parseInt(collection[i].substring(2));
+collection.splice(i,1);
+for (var j=0;j<num;j++)
+{
+collection.splice(i,0,ch);
+}
+}
+}
+  var count;
+  var alpha;
+  var ret=[];
+  var index=0;
+  var a;
+  for (i=0;i<collection.length;)
+  {
+	count=0;
+	alpha=collection[i];
+	do
+	{
+		count++;
+		i++;
+	}
+	while (i<collection.length&&collection[i]==alpha);
+	a=new Object();
+	a.name=alpha;
+	a.summary=count
+	ret[index++]=a;
+  }
+  return ret;
 }
