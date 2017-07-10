455b3d8d599f3327c7384083bddcd44f42b8ab69
lxxhack <1572411660@qq.com>
2017-07-10 17:04:56 +0800

    Update practice-4.js

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,48 @@
 'use strict';
-
+function countSameElements(collection) {
+  var count;
+  var alpha;
+  var ret=[];
+  var index=0;
+  var a;
+  for (var i=0;i<collection.length;)
+  {
+	count=0;
+	alpha=collection[i];
+	do
+	{
+		count++;
+		i++;
+	}
+	while (i<collection.length&&collection[i]==alpha);
+	a=new Object();
+	a.key=alpha;
+	a.count=count
+	ret[index++]=a;
+  }
+  return ret;
+}
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    for (var i=0;i<collectionA.length;i++)
+{
+if (collectionA[i].charAt(1)=='-')
+{
+var ch=collectionA[i].charAt(0);
+var num=collectionA[i].charAt(2);
+collectionA.splice(i,1);
+for (var j=0;j<num;j++)
+{
+collectionA.splice(i,0,ch);
+}
+}
+}
+collectionA=countSameElements(collectionA);
+  for (var i=0;i<collectionA.length;i++)
+  {
+    if (objectB.value.indexOf(collectionA[i].key)!=-1)
+    {
+      collectionA[i].count-=Math.floor(collectionA[i].count/3);
+    }
+  }
+  return collectionA;
 }
