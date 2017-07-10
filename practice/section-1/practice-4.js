c3dc90cfacafe53e8eca096a819fbdc9592add09
lxxhack <1572411660@qq.com>
2017-07-10 11:41:59 +0800

    Update practice-4.js

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,15 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+function collectSameElements(collectionA, collectionB) {
+  var ret=[];
+  var index=0;
+  for (var i=0;i<collectionA.length;i++)
+  {
+    if (collectionB.value.indexOf(collectionA[i].key)!=-1)
+    {
+      ret[index]=collectionA[i].key;
+      index++;
+    }
+  }
+  return ret;
 }
